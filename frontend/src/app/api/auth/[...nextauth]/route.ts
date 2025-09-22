86b0b2e4c57ed354735f50d02a85951399c6977f
MackenzyGreen <mackenzy.green95@gmail.com>
2025-09-21 20:20:02 -0500

    Start adding google oauth
    Started adding google oath via nextauth.js. Currently getting an error that I'm not sure is code related or google project setup related.

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID ?? "",
+            clientSecret: process.env.GOOGLE_SECRET ?? "",
+            authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          },
+        })
+    ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
