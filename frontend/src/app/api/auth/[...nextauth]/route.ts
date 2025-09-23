701da5e25023018ebaadd83b1edf812c9119055f
MackenzyGreen <mackenzy.green95@gmail.com>
2025-09-23 13:38:41 -0500

    minor changes

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -6,13 +6,6 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_ID ?? "",
             clientSecret: process.env.GOOGLE_SECRET ?? "",
-            authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code"
-            }
-          },
         })
     ],
 };
